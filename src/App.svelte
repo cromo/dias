2e528e5bfa11a00764d9896e79bc538a2f81b4f0
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-11 12:35:54 -0600

    Add ability to change start time
    This may result in data loss if filled purposes are sliced off either
    end when the adjustment is performed. If this would happen, there should
    be a confirmation prompt before doing it. But hey, MVP.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
-	import { newDay } from "./day";
+	import { newDay, adjustStartTime } from "./day";
 	import { makeSwatch, makePalette } from "./palette";
+	import DayGlance from "./DayGlance.svelte";
 	import TimeBlockDayView from "./TimeBlockDayView.svelte";
 	import TimeBlockDayEditor from "./TimeBlockDayEditor.svelte";
+
 	let palette = "creative";
+	let startTime = "00:00";
 
 	let blocks = newDay();
+	startTime = `${blocks.startTime.getHours().toString().padStart(2, '0')}:${blocks.startTime.getMinutes().toString().padStart(2, '0')}`;
 	let palettes = {
 		creative: makePalette("Creative", [
 			makeSwatch("reading", "#1df", /read/i),
@@ -22,6 +26,19 @@
 			makeSwatch("Work", "green", /work|code|debug|write/i),
 		])
 	};
+
+	function setNewStartTime() {
+		const match = startTime.match(/0?(\d+):(\d+)/);
+		if (!match) {
+			console.error("Bad input start time");
+			return;
+		}
+		const [, hours, minutes] = match;
+		const newStart = new Date(blocks.startTime);
+		newStart.setHours(+hours);
+		newStart.setMinutes(+minutes);
+		blocks = adjustStartTime(newStart, blocks);
+	}
 </script>
 
 {#each Object.keys(palettes) as p}
@@ -30,6 +47,7 @@
 	{p}
 </label>
 {/each}
+<input type=time bind:value={startTime}><button on:click={setNewStartTime}>Set Start Time</button>
 <TimeBlockDayView {blocks} palette={palettes[palette]}/>
 <DayGlance blocks={blocks.purposes} palette={palettes[palette]}/>
 <TimeBlockDayEditor bind:blocks palette={palettes[palette]}/>
--- src/day.js
+++ src/day.js
@@ -2,3 +2,25 @@ export function newDay(startTime) {
     startTime = startTime ? new Date(startTime) : new Date();
     return {startTime, purposes: new Array(100).fill("")};
 }
+
+// Shuffle around the existing purposes within the day so that the new block
+// that overlaps the most with the original block takes it's purpose. E.g. When
+// changing startTime from 6:00 to 6:04, the purpose at 7:00 gets assigned to
+// 7:04; if it went to 6:07 instead, it would get assigned to 6:57 instead.
+// This is equivalent to translating the times in the old day into block
+// indexes in the new day and rounding, then padding either side to make it 100
+// blocks again.
+export function adjustStartTime(newStartTime, day) {
+    const millisecondDelta = +newStartTime - day.startTime;
+    const blockDelta = Math.round(millisecondDelta / (10 * 60 * 1000));
+    const fillAtBeginning = blockDelta < 0;
+
+    const blankPurposes = new Array(Math.abs(blockDelta)).fill("");
+    const overlappingPurposes = fillAtBeginning ?
+        day.purposes.slice(0, blockDelta) :
+        day.purposes.slice(blockDelta);
+    const newPurposes = fillAtBeginning ?
+        [...blankPurposes, ...overlappingPurposes] :
+        [...overlappingPurposes, ...blankPurposes];
+    return {startTime: newStartTime, purposes: newPurposes};
+}
