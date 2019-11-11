cc719b6860bd42cae91b7e4afa4b0abe9121ea1f
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-10 22:48:10 -0600

    Use makePalette to make test palettes

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { makeSwatch } from "./palette";
+	import { makeSwatch, makePalette } from "./palette";
 	import TimeBlockDayView from "./TimeBlockDayView.svelte";
 	import TimeBlockDayEditor from "./TimeBlockDayEditor.svelte";
 	let palette = "creative";
 
 	let blocks = newDay();
 	let palettes = {
-		creative: [
+		creative: makePalette("Creative", [
 			makeSwatch("reading", "#1df", /read/i),
 			makeSwatch("writing", "green", /writ/i),
 			makeSwatch("debugging", "red", /debug/i),
@@ -15,11 +15,11 @@
 			makeSwatch("shopping", "yellow", /shopping|amazon/i),
 			makeSwatch("working", "gray", /work/i),
 			makeSwatch("sleeping", "navy", /sleep|nap|rest/i),
-		],
-		hnVsWork: [
+		]),
+		hnVsWork: makePalette("HN vs. Work", [
 			makeSwatch("Hacker News", "orange", /hn|hacker news|article/i),
 			makeSwatch("Work", "green", /work|code|debug|write/i),
-		]
+		])
 	};
 
 	function newDay(startTime) {
--- src/palette.js
+++ src/palette.js
@@ -1,5 +1,5 @@
 export function pickSwatch(palette, str) {
-    const matches = palette
+    const matches = palette.swatches
         .map(({color: c, pattern: p}) => ({color: c, match: str.match(p)}))
         .filter(x => x.match);
     matches.sort((a, b) => a.match.index - b.match.index);
