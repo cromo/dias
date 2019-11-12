68424ab2d8052cb5e0f6741d5fa2adbb97cfeca0
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-12 16:46:16 -0600

    Implement #/overview

--- src/routes/Overview.svelte
+++ src/routes/Overview.svelte
@@ -1,1 +1,9 @@
-Overview!
+<script>
+    import { daysDescending } from "../stores";
+    import DayGlance from "../DayGlance.svelte";
+    import { testPalettes } from "../palette";
+</script>
+
+{#each $daysDescending as day}
+    <DayGlance blocks={day.purposes} palette={testPalettes[0]}/>
+{/each}
