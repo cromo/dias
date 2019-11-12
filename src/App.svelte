6c37f2d5d2589e6135b7a4d52abfcbd570648640
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-12 16:56:30 -0600

    Implement #/edit/:date

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,7 @@
 	const routes = {
 		"/": Home,
 		"/overview": Overview,
-		"/edit": EditDay
+		"/edit/:date": EditDay
 	};
 
 	let palettes = testPalettes.reduce((palettes, palette) => {
--- src/routes/EditDay.svelte
+++ src/routes/EditDay.svelte
@@ -1,1 +1,10 @@
-Edit day!
+<script>
+    import { toIsoDate } from "../day";
+    import { days } from "../stores";
+    import DayEditor from "../DayEditor.svelte";
+    import { testPalettes } from "../palette";
+
+    export let params = {};
+</script>
+
+<DayEditor bind:day={$days[params.date]} palette={testPalettes[0]}/>
