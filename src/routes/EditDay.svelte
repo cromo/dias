b81afbdf62347187c50183d3fd1e7648b895deeb
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-12 18:20:31 -0600

    Thread palette selection into edit view

--- src/routes/EditDay.svelte
+++ src/routes/EditDay.svelte
@@ -1,10 +1,9 @@
 <script>
     import { toIsoDate } from "../day";
-    import { days } from "../stores";
+    import { days, palette } from "../stores";
     import DayEditor from "../DayEditor.svelte";
-    import { testPalettes } from "../palette";
 
     export let params = {};
 </script>
 
-<DayEditor bind:day={$days[params.date]} palette={testPalettes[0]}/>
+<DayEditor bind:day={$days[params.date]} palette={$palette}/>
