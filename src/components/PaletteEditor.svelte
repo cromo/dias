4aebc8bb563d7828006523bdad9d0c8454953126
Cristian Romo <cristian.g.romo@gmail.com>
2019-11-13 15:41:37 -0600

    Add pattern tester to palette editor

--- src/components/PaletteEditor.svelte
+++ src/components/PaletteEditor.svelte
@@ -1,5 +1,6 @@
 <script>
   import ColorBlock from "./ColorBlock.svelte";
+  import TimeBlockEditor from "./TimeBlockEditor.svelte";
   export let palette = {name: "", swatches: []};
 </script>
 
@@ -8,10 +9,28 @@
     display: grid;
     grid-template-columns: 40px 1fr 1fr 5fr;
     grid-gap: 4px 4px;
+    align-items: center;
+  }
+
+  .tester {
+    margin-left: 10%;
+    margin-right: 10%;
+    display: grid;
+    grid-template-columns: 40px 40px 1fr;
+    grid-gap: 4px 4px;
+    align-items: center;
+  }
+
+  div + div {
+    margin-top: 1.5em;
   }
 </style>
 
 <h1>{palette.name}</h1>
+
+<div class="tester">
+  <TimeBlockEditor colors={palette}/>
+</div>
 
 <div class="grid">
   {#each palette.swatches as swatch}
