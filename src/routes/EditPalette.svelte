c188e6762f309a6fd0881148311454d052acfb73
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-14 11:43:54 -0600

    Add adding palettes

--- src/routes/EditPalette.svelte
+++ src/routes/EditPalette.svelte
@@ -1,8 +1,15 @@
 <script>
+  import { replace } from "svelte-spa-router";
+  import { makePalette } from "../palette";
   import { rawPalettes } from "../stores";
   import PaletteEditor from "../components/PaletteEditor.svelte";
   export let params = {};
   let index = params.index;
+  if (index === "new") {
+    index = $rawPalettes.length;
+    $rawPalettes = [...$rawPalettes, makePalette("Unnamed", [])];
+    replace(`#/palette/${index}`);
+  }
 </script>
 
 <PaletteEditor bind:palette={$rawPalettes[index]}/>
